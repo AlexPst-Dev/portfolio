a60827b2afbea3bc34a44dbff159170f1ee5fb0a
AlexPst-Dev <alexandre-proust.dev@proton.me>
2025-12-12 16:40:35 +0100

    ✨ - add new home btn and update style

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,23 @@
 export default function Navbar() {
   return (
-    <nav className="flex w-full justify-end pr-6 mt-4">
+    <nav className="flex w-full justify-between py-8 px-32 sticky top-0 z-50 bg-neutral-900/20 backdrop-blur-md border-b border-neutral-800">
+      <a className="hover:underline" href="./">
+        Alexandre Proust
+      </a>
       <ul className="flex gap-8 list-none">
         <li className="hover:underline">
           <a href="./projects/">Projets</a>
         </li>
         <li className="hover:underline">
-          <a href="./degrees/">Diplômes</a>
-        </li>
-        <li className="hover:underline">
           <a href="./experiences/">Experiences</a>
+        </li>
+        <li>
+          <a
+            href="/contact/"
+            className="bg-white text-gray-900 px-4 py-2 rounded-md font-medium hover:bg-gray-100 transition-colors"
+          >
+            Contactez-moi
+          </a>
         </li>
       </ul>
     </nav>
